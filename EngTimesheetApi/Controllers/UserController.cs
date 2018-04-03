3c5d18fd57315d314e2d518c79e9f602547c0250
jkanawall <joey.kanawall@onsolve.com>
2018-04-03 14:32:26 -0400

    Start working on UserController

--- EngTimesheetApi/Controllers/UserController.cs
+++ EngTimesheetApi/Controllers/UserController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EngTimesheetApi.Data;
+using EngTimesheetApi.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EngTimesheetApi.Controllers
 {
@@ -11,6 +14,26 @@ namespace EngTimesheetApi.Controllers
 	[Route("api/User")]
 	public class UserController : Controller
 	{
+		private TimesheetContext _context;
+		private ITokenService _tokenService;
+		private ILogger _logger;
 
+		public UserController(TimesheetContext context, ITokenService tokenService, ILogger<AccountController> logger)
+		{
+			_context = context;
+			_tokenService = tokenService;
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			if(!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			return Ok(_context.Users.Select(x => x.FirstName));
+		}
 	}
 }
